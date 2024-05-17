8de6a9a5cb575ead52189a910cf3d00b5fe46658
patrick <17029142+colemanrpatrick@users.noreply.github.com>
2024-05-16 22:18:20 -0400

    update

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,17 @@
-let createSlideImage = (el,index) => {
+let createSlideImage = (el) => {
     if(!el.querySelector('IMG')){
         let dataImg = el.getAttribute("data-img");
         let slideImage = document.createElement("img");
         slideImage.src = dataImg;
-        slideImage.setAttribute("id","slide-image-" + index);
         el.appendChild(slideImage);
     };
 };
+let lazyimg = ($id) => {
+  let el = document.getElementById("" + $id + "");
+  createSlideImage(el);
+} 
 
+lazyimg("tour-slide-1");
 
 //ex: createSlideImage(slides[0],0);
 
